ae993b542bdf85479508d0d09ae23314c097647a
manny <mannymostafa123@gmil.com>
2024-12-04 23:00:23 +0200

    create type adabter for npte model

--- lib/models/notesapp_model.dart
+++ lib/models/notesapp_model.dart
@@ -1,8 +1,20 @@
-class NotesappModel {
+import 'package:hive/hive.dart';
+
+part 'notesapp_model.g.dart';
+@HiveType(typeId: 0)
+class NotesappModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subtitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
-  NotesappModel({required this.title, required this.subtitle, required this.date, required this.color}); 
+  NotesappModel(
+      {required this.title,
+      required this.subtitle,
+      required this.date,
+      required this.color});
 }
