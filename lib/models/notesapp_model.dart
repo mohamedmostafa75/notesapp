38362bb12cb833f03ac728a483e8ea7ff76ea239
manny <mannymostafa123@gmil.com>
2024-12-04 15:59:17 +0200

    create notes app model

---
+++ lib/models/notesapp_model.dart
@@ -0,0 +1,8 @@
+class NotesappModel {
+  final String title;
+  final String subtitle;
+  final String date;
+  final int color;
+
+  NotesappModel({required this.title, required this.subtitle, required this.date, required this.color}); 
+}
